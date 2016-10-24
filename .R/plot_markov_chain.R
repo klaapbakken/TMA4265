8c42c89c02700f5b11165976bb5c8b9165e0b6da
klaapbakken <oyvind.klaapbakken@gmail.com>
2016-10-24 20:35:59 +0200

    Updates from ShareLaTeX

--- .R/plot_markov_chain.R
+++ .R/plot_markov_chain.R
@@ -1,18 +1,20 @@
 #source("binomial_markov_chain.R")
-## ---- plot_markov_chain 
+## ---- plot_markov_chain_burn_in
 n <- 20
 p <- 0.3
 chain_length <- 5000
 X <- binomial_markov_chain(n, p, chain_length)
 
-qplot(c(0:199), X[1:200], geom="line", xlab = expression('X'[n]), ylab = "State")
+qplot(c(0:199), X[1:200], geom="step", xlab = expression('X'[n]), ylab = "State")
 
 ## ---- plot_markov_chain_histogram
 states <- c(0:n)
 ggplot() +
-  geom_bar(data = data.frame(X), aes(x = X, y = (..count..)/sum(..count..))) +
-  geom_point(data = data.frame(states), aes(x = states, y = dbinom(states, n, prob=p))) +
-  labs(x = "State", y = "Relative frequency") #+
+  geom_bar(data = data.frame(X), aes(x = X, y = (..count..)/sum(..count..)),
+           fill = "grey") +
+  geom_point(data = data.frame(states), 
+             aes(x = states, y = dbinom(states, n, prob=p))) +
+  labs(x = "State", y = "Relative frequency")
 
 ## ---- old
 # theme_bw()
